d8fb0ce3ca9a9a1e82856c7961c81575709afbf2
Saraswathi S <139524048+S-Saraswathi@users.noreply.github.com>
2023-08-19 19:18:29 +0530

    Add files via upload

---
+++ exp5_8.cpp
@@ -0,0 +1,26 @@
+//PRN: 22070123102
+//Exp-5
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i;
+    for(i=0;i<10;i++) {
+        cout<<"Hello World ("<<i+1<<")"<<endl;
+    }
+
+/*Output
+PS D:\Sem-3> cd "d:\Sem-3\" ; if ($?) { g++ exp5_8.cpp -o exp5_8 } ; if ($?) { .\exp5_8 }
+Hello World (1)
+Hello World (2)
+Hello World (3)
+Hello World (4)
+Hello World (5)
+Hello World (6)
+Hello World (7)
+Hello World (8)
+Hello World (9)
+Hello World (10) */
+
+}
